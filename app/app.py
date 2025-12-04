f81156f48295ad3b682cd0f293f2e366565c1a9c
anaaaraujoo <francisca.araujo1210@gmail.com>
2025-12-04 16:57:04 +0000

    Commit

--- app/app.py
+++ app/app.py
@@ -7,10 +7,10 @@ import db
 APP = Flask(__name__)
 
 # Start page
-@APP.route('/')
-def index():
-    # TODO
-    return render_template('index.html',message='Hello World!')
-
-# TODO 
-# ...
+@APP.route('/shows/')
+def shows():
+    shows = db.execute('''
+        SELECT show.title
+        FROM show
+    ''').fetchall()
+    return render_template('shows', shows = shows)
